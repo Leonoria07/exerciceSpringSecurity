b79766bc5be2f11913bcff7bc14072c9df137fa3
Leonoria07 <145671465+Leonoria07@users.noreply.github.com>
2024-01-22 10:42:58 +0100

    Update ExerciceSpringSecurityApplication.java

--- exerciceSpringSecurity/src/main/java/com/example/exercicespringsecurity/ExerciceSpringSecurityApplication.java
+++ exerciceSpringSecurity/src/main/java/com/example/exercicespringsecurity/ExerciceSpringSecurityApplication.java
@@ -11,7 +11,7 @@ public class ExerciceSpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExerciceSpringSecurityApplication.class, args);
-        UserService.createAdminUser("filsDuBoss","weshDu91");
+        UserService.createAdminUser("newAdmin","newAdminPass");
     }
 
 }
